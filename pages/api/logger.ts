ce450e4e4fa51e66379af3ec53de25a053c876a5
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-08-16 17:34:49 +0200

    test: logging

--- pages/api/logger.ts
+++ pages/api/logger.ts
@@ -1,9 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("log");
-  console.error("error");
-  console.info("info");
+  console.log("error");
 
   return res.json({ message: "logging" });
 };
