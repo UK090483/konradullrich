407cef8f8ebbb2e157865ac9e0c552ddf29b7264
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-09-15 12:43:28 +0200

    adding cypress

---
+++ cypress/e2e/pages.cy.ts
@@ -0,0 +1,37 @@
+describe("Pages ", () => {
+  it("passes", () => {
+    cy.visit("");
+    cy.get("a").each((page) => {
+      const link = page.prop("href") as string;
+      const isLink = !link.includes("mailto:");
+      if (!isLink) return;
+      cy.visit(link);
+
+      cy.get("a").each((page) => {
+        const link = page.prop("href") as string;
+        const isLink = !link.includes("mailto:");
+        //@ts-ignore
+        const isInternal = link.includes(Cypress.config().baseUrl);
+        if (!isLink) return;
+
+        if (isInternal) {
+          cy.visit(link);
+        }
+        if (!isInternal) {
+          cy.request({
+            url: link,
+            failOnStatusCode: false, // allow good and bad response to pass into then
+          }).then((response) => {
+            Cypress.log({
+              name: link,
+              message: response.status,
+            });
+          });
+        }
+
+        cy.log(JSON.stringify({ link, isInternal, isLink }));
+      });
+    });
+  });
+});
+export {};
