b9559ed5b829190c425377e623ef074d176512c3
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-10-06 14:45:34 +0200

    check e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "cypress run ",
+    baseUrl: "http://localhost:3000/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
